9c874eec03447a8b91a7bd3d89be8d26901b67f0
Qureshi <Ammad.Qureshi@ceridian.com>
2022-05-19 01:43:42 -0400

    added name input and choices for priority lvls

--- app.js
+++ app.js
@@ -4,18 +4,44 @@ let itemNumber = 1;
 function addItem(){
 	let table = document.getElementById('table');
 	let row = table.insertRow(rows++);
+	
 	let itemNumberCell = row.insertCell(0);
-	itemNumberCell.innerText = itemNumber++;
+	itemNumberCell.innerText = itemNumber;
+	
 	let nameCell = row.insertCell(1);
-	nameCell.innerText = "Item "+(itemNumber-1);
+	let name = document.createElement('INPUT');
+	name.setAttribute('type', 'text');
+	name.placeholder = 'Item ' + itemNumber;
+	nameCell.append(name);
+	
 	let priorityCell = row.insertCell(2);
-	priorityCell.innerText = "1 - Low";
+	priorityCell.append(priorityFunction());
 
-	let del = document.createElement('button');
-	del.innerText = "Delete";
-	let delCell = row.insertCell(3);
+	itemNumber++;
+
+}
+
+function priorityFunction(){
+	let priority = document.createElement('select');
+	priority.name = 'priorityLevel';
+	priority.id = 'priorityLevel';
+
+	let op1 = document.createElement('option');
+	op1.value = 1; op1.innerText = 'Low';
+
+	let op2 = document.createElement('option');
+	op2.value = 5; op2.innerText = 'Medium';
 	
-	delCell.append(del);
+	let op3 = document.createElement('option');
+	op3.value = 10; op3.innerText = 'High';
+
+	priority.append(op1);
+	priority.append(op2);
+	priority.append(op3);
+
+	return priority;
+}
+
+function deleteItem(itemNumberToBeDeleted){
 	
-	// table.insertRow(rows++);
 }
